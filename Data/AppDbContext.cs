963d701f54fbf70745f516e1b11cd003553844c4
Matis <matis@matis.io>
2024-11-01 10:53:31 -0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelosMigracionesEnAula.Models;  
+
+namespace ModelosMigracionesEnAula.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Define el DbSet para tu modelo
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
